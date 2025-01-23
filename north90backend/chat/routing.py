f1294c40e053d9e1139e18a04f4b459e491e10eb
substantialcattle5 <nilaynathsharan16@gmail.com>
2025-01-23 17:13:44 +0530

    refactor the backend task

--- north90backend/chat/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-
-from chat.comsumer import ChatConsumer
-
-websocket_urlpatterns = [
-    re_path(r'wss/chat/(?P<sender_id>\d+)(?P<receiver_id>\d+)/$', ChatConsumer.as_asgi()),
-]
